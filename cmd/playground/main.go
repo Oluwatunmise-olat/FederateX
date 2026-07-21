6f7e54498f0162ff96344c8301846f7c0c01712b
Oluwatunmise-olat <oluwatunmiseolatunbosun2001@gmail.com>
2026-07-21 02:21:52 +0100

    Consume message packet and understand payload sent

--- cmd/playground/main.go
+++ cmd/playground/main.go
@@ -11,8 +11,34 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	
+
 	defer tun.Close()
 
 	fmt.Println("Created TUN interface: federatex0")
+
+	fmt.Println("waiting for packets... (press Ctrl+C to stop)")
+
+	maxPacketSize := 1500
+
+	packetBuffer := make([]byte, maxPacketSize)
+
+	for {
+		n, err := tun.Read(packetBuffer)
+
+		if err != nil {
+			log.Fatalf("read from tun: %v", err)
+		}
+
+		packet := packetBuffer[:n]
+
+		version := packet[0] >> 4
+
+		protocol := packet[9]
+
+		srcIP := fmt.Sprintf("%d.%d.%d.%d", packet[12], packet[13], packet[14], packet[15])
+
+		dstIP := fmt.Sprintf("%d.%d.%d.%d", packet[16], packet[17], packet[18], packet[19])
+
+		fmt.Printf("IPv%d | From: %s -> To: %s | protocol=%d | %d bytes\n", version, srcIP, dstIP, protocol, n)
+	}
 }
